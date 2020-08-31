087a2b29e510c97901edd2d265c5624dee525e87
Administrator <qq844303749@gmail.com>
2020-08-31 23:11:12 +0800

    new front

--- src/main/kotlin/xyz/luchengeng/authentication/TypeScriptGen.kt
+++ src/main/kotlin/xyz/luchengeng/authentication/TypeScriptGen.kt
@@ -9,7 +9,7 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
 
-fun main(){
+fun gen(){
     println(TypeScriptGenerator(
             rootClasses = setOf(
                     Antique::class,
